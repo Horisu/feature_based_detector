e5f41b482977c42e6e378c7181855d92c0fdb942
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-06-28 16:11:55 +0900

    publish debug image

--- src/feature_based_detector_node.cpp
+++ src/feature_based_detector_node.cpp
@@ -25,10 +25,11 @@ std::vector<cv::KeyPoint> ref_kp_;
 std::vector<cv::Point2f> bb_;
 
 // ros
-ros::Publisher pub_;
+ros::Publisher pub_rect_;
+ros::Publisher pub_debug_;
 
 // parameters
-const double akaze_thresh = 1e-4; // AKAZE detection threshold set to locate about 1000 keypoints
+const double akaze_thresh = 1e-5; // AKAZE detection threshold set to locate about 1000 keypoints
 const double ransac_thresh = 2.5f; // RANSAC inlier threshold
 const double nn_match_ratio = 0.8f; // Nearest-neighbour matching ratio
 const int bb_min_inliers = 10; // Minimal number of inliers to draw bounding box
@@ -82,6 +83,16 @@ void resetKeyPoints(cv::Mat &_image){
 };
 
 void process(const cv::Mat& _image, const std_msgs::Header& _header) {
+  cv::Mat image_res = _image.clone(); //for debug
+  sensor_msgs::Image image_msg;
+  image_msg.header = _header;
+  image_msg.height = _image.size().height;
+  image_msg.width = _image.size().width;
+  image_msg.encoding = "rgb8";
+  image_msg.step = 3 * image_msg.width;
+  image_msg.data.reserve(image_msg.height * image_msg.width * 3);
+  geometry_msgs::PolygonStamped res;
+  res.header = _header;
   std::vector<cv::KeyPoint> kp;
   cv::Mat desc;
   detector_->detectAndCompute(_image, cv::noArray(), kp, desc);
@@ -96,6 +107,8 @@ void process(const cv::Mat& _image, const std_msgs::Header& _header) {
     }
   }
 
+  ROS_INFO("mathed points %d", static_cast<int>(matched1.size()));
+
   cv::Mat inlier_mask, homography;
   std::vector<cv::KeyPoint> inliers1, inliers2;
   std::vector<cv::DMatch> inlier_matches;
@@ -104,6 +117,15 @@ void process(const cv::Mat& _image, const std_msgs::Header& _header) {
                                 cv::RANSAC, ransac_thresh, inlier_mask);
   }
   if(matched1.size() < 4 || homography.empty()) {
+    for (int i = 0; i < image_msg.height; ++i) {
+      for (int j = 0; j < image_msg.width; ++j) {
+        image_msg.data.push_back(image_res.at<cv::Vec3b>(i,j)[2]);//r
+        image_msg.data.push_back(image_res.at<cv::Vec3b>(i,j)[1]);//g
+        image_msg.data.push_back(image_res.at<cv::Vec3b>(i,j)[0]);//b
+      }
+    }
+    pub_rect_.publish(res);
+    pub_debug_.publish(image_msg);
     return;
   }
   for(unsigned i = 0; i < matched1.size(); i++) {
@@ -119,21 +141,33 @@ void process(const cv::Mat& _image, const std_msgs::Header& _header) {
   perspectiveTransform(bb_, new_bb, homography);
   if((int)inliers1.size() >= bb_min_inliers) {
     // object found
-    geometry_msgs::PolygonStamped res;
-    res.header = _header;
     res.polygon.points.reserve(2);
     geometry_msgs::Point32 point_lu, point_rd;
     point_lu.x = new_bb[0].x;
     point_lu.y = new_bb[0].y;
-    point_rd.x = new_bb[1].x;
-    point_rd.y = new_bb[1].y;    
+    point_rd.x = new_bb[2].x;
+    point_rd.y = new_bb[2].y;    
     res.polygon.points.push_back(point_lu);
     res.polygon.points.push_back(point_rd);
-    pub_.publish(res);
+
+    //cv::rectangle(image_res, new_bb[0], new_bb[2], cv::Scalar(255,0,0), 2, 1);
+    cv::line(image_res, new_bb[0], new_bb[1], cv::Scalar(255,0,0), 2, 1);
+    cv::line(image_res, new_bb[1], new_bb[2], cv::Scalar(255,0,0), 2, 1);
+    cv::line(image_res, new_bb[2], new_bb[3], cv::Scalar(255,0,0), 2, 1);
+    cv::line(image_res, new_bb[3], new_bb[0], cv::Scalar(255,0,0), 2, 1);
   }
 
+  // mat to msg
+  for (int i = 0; i < image_msg.height; ++i) {
+    for (int j = 0; j < image_msg.width; ++j) {
+      image_msg.data.push_back(image_res.at<cv::Vec3b>(i,j)[2]);//r
+      image_msg.data.push_back(image_res.at<cv::Vec3b>(i,j)[1]);//g
+      image_msg.data.push_back(image_res.at<cv::Vec3b>(i,j)[0]);//b
+      }
+  }
+  pub_rect_.publish(res);
+  pub_debug_.publish(image_msg);
   return;
-      
 };
 
 void imageCallback(const sensor_msgs::Image::ConstPtr& _image_msg){
@@ -156,25 +190,25 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& _image_msg){
     resetKeyPoints(image);
     reset_ = false;
   }
-  
   process(image, _image_msg->header);
 };
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "feature_based_detector");
   ros::NodeHandle nh;
-  pub_ = nh.advertise<geometry_msgs::PolygonStamped>("/object/detected", 100);
 
   detector_ = MY_DETECTOR::create();
+  detector_->setThreshold(akaze_thresh);
   matcher_ = cv::DescriptorMatcher::create("BruteForce-Hamming");
 
   active_ = false;
   reset_ = false;
 
-  ros::Subscriber  bb_sub_ = nh.subscribe("input_bb", 3, bbCallback);
-  ros::Subscriber image_sub_ = nh.subscribe("input", 3, imageCallback);
+  ros::Subscriber  bb_sub_ = nh.subscribe("input/bb", 1, bbCallback);
+  ros::Subscriber image_sub_ = nh.subscribe("input/image", 1, imageCallback);
 
-  pub_ = nh.advertise<geometry_msgs::PolygonStamped>("output",1000);
+  pub_rect_ = nh.advertise<geometry_msgs::PolygonStamped>("output",1000);
+  pub_debug_ = nh.advertise<sensor_msgs::Image>("output/debug",1000);
   ros::spin();
   return 0;
 }
